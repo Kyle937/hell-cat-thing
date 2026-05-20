5089f7d1ef56382d9d588144898ba66dc3818c25
Kyle937 <kylekreeper@gmail.com>
2026-05-20 13:38:00 -0400

    small fix

--- Main.py
+++ Main.py
@@ -4,12 +4,12 @@ class HellCat:
         # initalizing
 
         if color.strip().isalpha():
-            self.__color__ = color.strip()
+            self.__color = color.strip()
 
         if eyeColor.strip().isalpha():
-            self.__eyeColor__ = eyeColor.strip()
+            self.__eyeColor = eyeColor.strip()
 
-        self.__wearing__ = True
+        self.__wearing = True
 
     def __str__(self):
         # incase of print
@@ -18,23 +18,27 @@ class HellCat:
 
     def toggleOn(self):
         # taking off/putting on
-        if self.__wearing__:
-            self.__wearing__ = False
+        if self.__wearing:
+            self.__wearing = False
 
         else:
-            self.__wearing__ = True
+            self.__wearing = True
 
 
     def isOn(self):
         # checks if on
-        return self.__wearing__
+        return self.__wearing
     
+
+    def checkEye(self):
+        return self.__eyeColor
+
 
     def dye(self,new):
         # changing color
         if new.strip().isalpha():
-            self.__color__ = new.strip()
-            print("badge color changed to", self.__color__)
+            self.__color = new.strip()
+            print("badge color changed to", self.__color)
 
 
 bob = HellCat(input("body color: "), input("eye color: "))
